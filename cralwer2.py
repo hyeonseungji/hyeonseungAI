a4b56e7b93e1805ed648f8f998f0c0494def0d61
hyeonseungji <46896093+hyeonseungji@users.noreply.github.com>
2019-02-02 13:51:38 +0900

    Add files via upload

---
+++ cralwer2.py
@@ -0,0 +1,34 @@
+import urllib.request
+
+year = 2012
+month = 6
+day = 1
+hour = 0
+minute = "00"
+
+for month in range(6,13):
+    if month in (6,9,11):
+        for day in range(29,31):
+            for hour in range(0,24,6):
+                form = str(year) + str(month).zfill(2) + str(day).zfill(2) + str(hour).zfill(2) + minute
+                url = "https://images.lightningmaps.org/blitzortung/america/index.php?map=usa_big&date=" + form + "-360"
+                print(url)
+                name = form + ".png"
+                mem = urllib.request.urlopen(url).read()
+
+                with open(name,mode="wb") as f:
+                    f.write(mem)
+
+    if month in (7,8,10,12):
+        for day in range(29,32):
+            for hour in range(0,24,6):
+                form = str(year) + str(month).zfill(2) + str(day).zfill(2) + str(hour).zfill(2) + minute
+                url = "https://images.lightningmaps.org/blitzortung/america/index.php?map=usa_big&date=" + form + "-360"
+                print(url)
+                name = form + ".png"
+                mem = urllib.request.urlopen(url).read()
+
+                with open(name,mode="wb") as f:
+                    f.write(mem)
+        
+
